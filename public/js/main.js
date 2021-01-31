c7d744f15de77662a667480ae8385e7160b7105b
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:21:27 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+// exo1
+
+// for (let i = 0; i <= 20; i++) {
+//     // console.log(i);
+//     if (i % 2 == 0) {
+//         console.log(i, "nombre paire");
+//     } else if (i % 1 == 0) {
+//         console.log(i, "nombre impaire");
+//     }
+// }  
+
+
+// exo2
+
+// let coding16 = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"]
+
+// for (let i = 0; i < 18; i++) {
+//     console.log("bonjour " + coding16[i]);    
+// }
+
+// exo3
+
+// let nombre = prompt("insere un nombre")
+// for (let i = 0; i <= nombre; i++) {
+//     console.log(i);    
+// }
